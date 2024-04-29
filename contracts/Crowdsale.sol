8020e95c069e0c5ec3d802b6d44b08ed49b692f3
jakkie <jakubkielbasiewicz@gmail.com>
2024-04-30 00:27:15 +0200

    finalize init

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -6,12 +6,17 @@ contract Crowdsale {
 
     Token public token;
     uint256 public price;
+    address public owner;
+    uint256 public maxTokens;
+    uint256 public tokensSold;
 
     event Buy(uint256 amount, address buyer);
 
-    constructor(Token _token, uint256 _price) {
+    constructor(Token _token, uint256 _price, uint256 _maxTokens) {
+        owner = msg.sender;
         token = _token;
         price = _price;
+        maxTokens = _maxTokens;
     }
 
     receive() external payable {
@@ -24,7 +29,16 @@ contract Crowdsale {
         require(token.balanceOf(address(this)) >= _amount, "Not enough tokens in the contract");
         require(token.transfer(msg.sender, _amount));
 
+        tokensSold += _amount;
+
         emit Buy(_amount, msg.sender);
+    }
+
+    function finalize() public {
+        // send remaining tokens to crowdsale creator
+        uint256 remainingTokens = token.balanceOf(address(this));
+        token.transfer(owner, remainingTokens);
+        // send eth to crowdsale creator
     }
 
 }
