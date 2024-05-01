6b64a2d1ca6f3180392a824e865fe4fe7b365b05
jakkie <jakubkielbasiewicz@gmail.com>
2024-05-01 23:45:22 +0200

    deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,7 +1,12 @@
 const hre = require("hardhat");
 
 async function main() {
-    hre.ethers.getContractFactory("Token")  
+    const Token = hre.ethers.getContractFactory("Token");
+    let token = await Token.deploy("just TOKEN", "JUST", 1000000000000000000000000);
+    await token.deployed();
+
+    console.log("Token deployed to:", token.address);
+
 }
 
 main().catch((error) => {
