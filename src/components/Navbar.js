42ea99847b5b972e3fdd3b0f9398927cb1c2b2be
jakkie <jakubkielbasiewicz@gmail.com>
2024-05-03 00:01:41 +0200

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,14 @@
-import { Container } from "react-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
 
 import logo from "../just_token.svg";
 
-function App() {
+const Navigation = () => {
     return (
-        <Container>
             <Navbar>
                 <img alt="logo" src={logo} width="40" height="40" className="d-inline-block align-top mx-3"/>
                 <Navbar.Brand href="#">JUST TOKEN Crowdsale</Navbar.Brand>
             </Navbar>
-        </Container>
     );
 }
 
-export default App;
+export default Navigation;
