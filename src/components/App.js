0b62271749bb2e6cf699d06b9fd49f8d7c2d534e
jakkie <jakubkielbasiewicz@gmail.com>
2024-05-02 23:53:27 +0200

    web app init

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import { Container } from "react-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
+
+function App() {
+    return (
+        <Container>
+            <Navbar>
+                <Navbar.Brand href="#">JUST TOKEN Crowdsale</Navbar.Brand>
+            </Navbar>
+        </Container>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
-const root = reactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <App />
