b6cc519e39ac47e301b70594c8a95ed080e295d4
jakkie <jakubkielbasiewicz@gmail.com>
2024-05-09 16:05:30 +0200

    get contract

--- src/App.js
+++ src/App.js
@@ -5,27 +5,38 @@ import { useEffect, useState } from "react";
 import Navigation from "./components/Navigation";
 import Info from "./components/Info";
 
+import TOKEN_ABI from './abis/Token.json'
+import CROWDSALE_ABI from './abis/Crowdsale.json'
+
 
 
 function App() {
 
     const [account, setAccount] = useState(null);
     const [provider, setProvider] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
 
     const loadBlockchainData = async () => {
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         setProvider(provider);
-        
+
+        const token = new ethers.Contract('0x5fbdb2315678afecb367f032d93f642f64180aa3', TOKEN_ABI, provider);
+        console.log(token)
 
         const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
         const account = ethers.utils.getAddress(accounts[0]);
         setAccount(account);
 
+        setIsLoading(false);
+
     }
 
     useEffect(() => {
-        loadBlockchainData();
-    });
+        if (isLoading) {
+            loadBlockchainData();
+        
+        }
+    }, [isLoading]);
     return (
         <Container>
             <Navigation />
