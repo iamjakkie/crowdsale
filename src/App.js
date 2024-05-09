4dcb68aff08d71d92bafd3cda174368ce980b5ca
jakkie <jakubkielbasiewicz@gmail.com>
2024-05-09 15:59:13 +0200

    abis

--- src/App.js
+++ src/App.js
@@ -10,10 +10,12 @@ import Info from "./components/Info";
 function App() {
 
     const [account, setAccount] = useState(null);
+    const [provider, setProvider] = useState(null);
 
     const loadBlockchainData = async () => {
-        let provider = new ethers.providers.Web3Provider(window.ethereum);
-        console.log(provider);
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        setProvider(provider);
+        
 
         const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
         const account = ethers.utils.getAddress(accounts[0]);
