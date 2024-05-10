d47fa5b8aea7c472393345aea01d04e259075220
jakkie <jakubkielbasiewicz@gmail.com>
2024-05-10 16:03:33 +0200

    buy logic finish

--- src/components/Buy.js
+++ src/components/Buy.js
@@ -3,21 +3,50 @@ import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Spinner from 'react-bootstrap/Spinner';
+import { useState } from 'react';
+import { ethers } from 'ethers';
 
 const Buy = ({ provider, price, crowdsale, setIsLoading }) => {
+
+    const [amount, setAmount] = useState(0);
+    const [isWaiting, setIsWaiting] = useState(false);
+
+    const buyHandler = async (e) => {
+        e.preventDefault();
+        setIsWaiting(true);
+        try {
+            const value = ethers.utils.parseUnits((amount * price).toString(), 'ether');
+            const formattedAmount = ethers.utils.parseUnits(amount.toString(), 'ether');
+            const signer = await provider.getSigner();
+
+            const transaction = await crowdsale.connect(signer).buyTokens(formattedAmount, { value: value });
+            await transaction.wait()
+        } catch {
+            window.alert("User rejected or transaction reverted");
+        }
+        
+
+        setIsLoading(true);
+    }
+
     return (
-        <Form style={{ maxWidth: '800px', margin: '50px auto'}}>
+        <Form onSubmit={buyHandler} style={{ maxWidth: '800px', margin: '50px auto' }}>
             <Form.Group as={Row}>
                 <Col>
-                <Form.Control type="number" placeholder="Amount" />
+                    <Form.Control type="number" placeholder="Amount" onChange={(e) => setAmount(e.target.value)} />
                 </Col>
                 <Col className='text-center'>
-                <Button variant='primary' type='Submit' style={{ width: '100%'}}>
+                    {isWaiting ? (
+                        <Spinner animation="border"/>
+                    ): (
+                            <Button variant = 'primary' type = 'Submit' style = {{ width: '100%' }}>
                     BUY
                 </Button>
-                </Col>
-            </Form.Group>
-        </Form>
+                    )}
+
+            </Col>
+        </Form.Group>
+        </Form >
     )
 }
 
